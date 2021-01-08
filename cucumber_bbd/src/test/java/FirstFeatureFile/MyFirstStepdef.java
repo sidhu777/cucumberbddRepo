89bc71cfc09e19abaa7114f1f5935fd7ea7e8f1f
siddanth.chilla <siddanth.chilla@DSHYD-113.testingxperts.loc>
2021-01-08 13:41:42 +0530

    Third commit

--- cucumber_bbd/src/test/java/FirstFeatureFile/MyFirstStepdef.java
+++ cucumber_bbd/src/test/java/FirstFeatureFile/MyFirstStepdef.java
@@ -21,6 +21,7 @@ public class MyFirstStepdef {
 		driver.get("https://www.facebook.com/");
 		 driver.manage().window().maximize();
 		 System.out.println("opening the url");
+		 System.out.println("make my trip -------------------------------------");
 		}
 			
 	
